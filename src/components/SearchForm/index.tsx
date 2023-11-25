b6ef442c598a5b45a5175ff341708baaaf5621c7
GABRIEL PEDRO RAMALHO <biel-pedro@hotmail.com>
2023-11-25 16:33:02 -0300

    Add @hookform/resolvers and react-hook-form dependencies

--- src/components/SearchForm/index.tsx
+++ src/components/SearchForm/index.tsx
@@ -1,13 +1,39 @@
+import * as z from 'zod';
+
 import { FormContainer } from "./styles";
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+
+
+const searchFormSchema = z.object({
+    query: z.string(),
+})
+
+
+type SearchFormInputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm(){
+
+    const {register, handleSubmit} = useForm<SearchFormInputs>({
+        resolver: zodResolver(searchFormSchema),
+    })
+
+    function handleSearchPost(data: SearchFormInputs){
+        console.log(data)
+    }
+
     return(
-        <FormContainer>
+        <FormContainer onSubmit={handleSubmit(handleSearchPost)}>
             <div>
                 <span>Publicações</span>
                  <span>6 publicações</span>
             </div>
-            <input type="text" placeholder="Buscar conteúdo" />
+            <input 
+                type="text"
+                placeholder="Buscar conteúdo" 
+                {...register('query')}
+            />
         </FormContainer>
     )
 }
